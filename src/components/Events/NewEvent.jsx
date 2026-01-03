2d64d85b422972433622b4b445c6c76b746566af
Steve Tyler <steve.tyler75@gmail.com>
2026-01-03 15:33:16 +0000

    sectino 417

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,43 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
+  
+  // mutation will not be executed until we call mutate
+  const { mutate, isPending, isError, error } = useMutation({
+    // mutationKey not needed here as we are not using it to identify/cache the mutation
+    mutationFn: createNewEvent,
+  });
 
-  function handleSubmit(formData) {}
+
+  function handleSubmit(formData) {
+    mutate({
+      event: formData
+    })
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && 'Submitting...'}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && <ErrorBlock title="Failed to create event" message={error.info?.message || 'Failed to create event, please check and try again.'} />}
     </Modal>
   );
 }
--- src/util/http.js
+++ src/util/http.js
@@ -1,18 +1,43 @@
 export async function fetchEvents({ signal, searchTerm }) {
-    let URL = 'http://localhost:3000/events';
-    if (searchTerm) {
-        URL += `?search=${encodeURIComponent(searchTerm)}`;
-    }   
-    const response = await fetch(URL, { signal }); // Pass the signal to fetch for aborting
+  console.log(searchTerm);
+  let url = 'http://localhost:3000/events';
 
-    if (!response.ok) {
+  if (searchTerm) {
+    url += '?search=' + searchTerm;
+  }
+
+  const response = await fetch(url, { signal: signal });
+
+  if (!response.ok) {
     const error = new Error('An error occurred while fetching the events');
     error.code = response.status;
     error.info = await response.json();
     throw error;
-    }
+  }
 
-    const { events } = await response.json();
+  const { events } = await response.json();
 
-    return events;
+  return events;
+}
+
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while creating the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
 }
