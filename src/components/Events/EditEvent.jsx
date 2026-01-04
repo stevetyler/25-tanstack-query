303952bf8c38a282bf516d582259e84cfb0c0094
Steve Tyler <steve.tyler75@gmail.com>
2026-01-04 17:38:02 +0000

    section 424

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,10 +1,21 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
+import { fetchEvent } from '../../util/http.js';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['event', params.id], // unique key for editing event
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    staleTime: 60000, // cache for 1 minute
+  });
 
   function handleSubmit(formData) {}
 
@@ -12,16 +23,42 @@ export default function EditEvent() {
     navigate('../');
   }
 
+  let content;
+
+  if (isPending) {
+    content = <div className='center'>
+      <LoadingIndicator />
+    </div>;
+  }
+
+  if (isError) {
+    content = <>
+      <ErrorBlock title="Failed to load event" message={error.info?.message || 'Failed to fetch event details.'} />
+      <div className="form-actions">
+        <Link to="../" className="button">
+          OK
+        </Link>
+      </div>
+    </>
+  }
+  if (data) {
+    content = (
+      <>
+        <EventForm inputData={data} onSubmit={handleSubmit}>
+          <Link to="../" className="button-text">
+            Cancel
+          </Link>
+          <button type="submit" className="button">
+            Update
+          </button>
+        </EventForm>
+      </>
+    )
+  }
+
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {content}
     </Modal>
   );
 }
